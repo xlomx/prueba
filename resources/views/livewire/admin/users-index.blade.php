80bee4fb8cd142c6bebfad597e45fbf23ed108da
Carlos Morales <carlosluismorales1@gmail.com>
2022-12-09 05:19:10 -0500

    primer

---
+++ resources/views/livewire/admin/users-index.blade.php
@@ -0,0 +1,16 @@
+<div>
+   
+
+  
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+
+                <div class="bg-green-100 border-t border-b border-green-500 text-green-700 px-4 py-3" role="alert">
+                    <p class="font-bold">Informational message</p>
+                    <p class="text-sm">Some additional text to explain said message.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
